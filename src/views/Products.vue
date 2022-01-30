a26a9736d7231a657c3b91f357b715ad8a2e8f7b
Yerassyl Bekberov <bekberata@gmail.com>
2022-01-30 15:40:59 +0600

    feat: complete the tutorial

---
+++ src/views/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+    <div class="card-container">
+      <ProductCard
+        v-for="(product, index) in inventory"
+        :key="product.id"
+        class="card"
+        :product="product"
+        :index="index"
+        :addToCart="addToCart"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
